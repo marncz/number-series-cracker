0e316dd5c65ba6c906a1647a0b752b813e5e636e
Marcin <marncz@users.noreply.github.com>
2015-09-25 00:29:53 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,27 +4,27 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-		String input = "";
-		while(!input.equals("END")){
-        System.out.print("Enter series: ");
-        input = BIO.getString();
-        System.out.println();
+        String input = "";
+        while (!input.equals("END")) {
+            System.out.print("Enter series: ");
+            input = BIO.getString();
+            System.out.println();
 
-        input = input.replaceAll("\\s+", ""); // Removes all spaces
-        input = input.replaceAll("\\;+", ","); // Changes ; to commas
-      
-        String[] array = input.split(",");
-        ArrayList < Double > int_list = new ArrayList < Double > ();
+            input = input.replaceAll("\\s+", ""); // Removes all spaces
+            input = input.replaceAll("\\;+", ","); // Changes ; to commas
 
-        for (int i = 0; i < array.length; i++) {
-            int_list.add(Double.parseDouble(array[i]));
+            String[] array = input.split(",");
+            ArrayList < Double > int_list = new ArrayList < Double > ();
+
+            for (int i = 0; i < array.length; i++) {
+                int_list.add(Double.parseDouble(array[i]));
+            }
+
+            Cracker serie = new Cracker(int_list);
+
         }
 
-	Cracker serie = new Cracker(int_list);
-	
-	}
-   
     }
 
-  
+
 }
